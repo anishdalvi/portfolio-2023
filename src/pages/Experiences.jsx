ea3481b8c0b14eff9c7b364ff252bf5454ac6542
anishDalvi <anishdalvi2001@gmail.com>
2023-07-20 17:33:16 +0530

    Edited TimeLine Date Color

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -29,7 +29,9 @@ const Experiences = ({theme}) => {
                                 <div className="cursor-default text-sm sm:text-base font-semibold text-slate-800 dark:text-slate-400 sm:mb-0" key={experience.id}> {experience.post} </div>
                                 {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
                                 <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-400  sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-light-sign  dark:after:bg-dark-sign  after:border-4 after:box-content after:border-slate-500 dark:after:border-slate-200 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs sm:text-sm font-semibold uppercase w-20 h-5 sm:w-20 sm:h-6 mt-1 sm:mt-0 mb-2 sm:mb-0 text-light-sign bg-dark-sign dark:text-dark-sign dark:bg-light-sign rounded-full">{experience.date}</time>
+
+                                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs sm:text-sm font-semibold uppercase w-20 h-5 sm:w-20 sm:h-6 mt-1 sm:mt-0 mb-2 sm:mb-0 text-blue-800 bg-blue-300 dark:text-blue-300 dark:bg-blue-800 rounded-full">{experience.date}</time>
+
                                     <div className="cursor-pointer text-lg sm:text-xl font-bold text-dark-sign dark:text-light-sign  ">{experience.company}</div>
                                 </div>
                                 {/* <!-- Content --> */}
